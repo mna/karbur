96ed1bd86f60de1b4580415d2f1cc2f2006666b0
Martin Angers <martin.n.angers@gmail.com>
2026-05-18 17:53:56 -0400

    Webpages: initial draft

---
+++ server/webpages/webpages.go
@@ -0,0 +1,27 @@
+package webpages
+
+type Renderer struct {
+	pages map[string]executer
+}
+
+type executer interface {
+	Execute(io.Writer, any) error
+}
+
+// New creates a new Renderer with templates from the provided filesystem. The
+// common/ sub-directory must contain reusable templates (e.g. layouts,
+// partials), while the pages/ sub-directory contains the entrypoints (actual
+// pages to render). Template names are the path relative to the initial
+// sub-directory, e.g. "common/layouts/base.tpl" would be compiled to a
+// template named "layouts/base.tpl", while "pages/app/login.tpl" would be
+// named "app/login.tpl".
+func New(tpls fs.FS) (*Renderer, error) {
+}
+
+func (r *Renderer) Render(w io.Writer, page string, data any) error {
+	p, ok := r.pages[page]
+	if !ok {
+		return errors.Errorf("webpages: no such page: %q", page)
+	}
+	return p.Execute(w, data)
+}
