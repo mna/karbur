6288a8993478f9cc71d066263e39cf3ac39f1c29
Martin Angers <martin.n.angers@gmail.com>
2026-05-25 18:08:21 -0400

    Finalize testing tokens

--- tokens/tokens_test.go
+++ tokens/tokens_test.go
@@ -70,6 +70,96 @@ func TestPool(t *testing.T) {
 			_, err = Verify(ctx, pool, tok1, MustMatchTypeAndRefID("test", 1))
 			require.Error(t, err)
 			require.ErrorIs(t, err, ErrInvalid)
+
+			// create another single-use token
+			tok2a, err := New(ctx, pool, TokenArgs{
+				Type:      "test",
+				RefID:     2,
+				SingleUse: true,
+				Expiry:    time.Minute,
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, tok2a)
+
+			// generate another for the same type/ref, will replace it
+			tok2b, err := New(ctx, pool, TokenArgs{
+				Type:      "test",
+				RefID:     2,
+				SingleUse: true,
+				Expiry:    time.Minute,
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, tok2b)
+			require.NotEqual(t, tok2a, tok2b)
+
+			// verify the initial token, invalid
+			_, err = Verify(ctx, pool, tok2a, nil)
+			require.ErrorIs(t, err, ErrInvalid)
+
+			// verify the new token, valid
+			_, err = Verify(ctx, pool, tok2b, nil)
+			require.NoError(t, err)
+
+			// generate a multi-use token
+			tok3, err := New(ctx, pool, TokenArgs{
+				Type:      "test",
+				RefID:     3,
+				SingleUse: false,
+				Expiry:    time.Minute,
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, tok3)
+
+			// verify it, valid
+			dbtok, err = Verify(ctx, pool, tok3, MustMatchType("test"))
+			require.NoError(t, err)
+			require.Equal(t, tok3, dbtok.Token)
+			require.EqualValues(t, 3, dbtok.RefID)
+
+			// verify it with a non-matching type, invalid
+			_, err = Verify(ctx, pool, tok3, MustMatchType("NO-SUCH-TYPE"))
+			require.ErrorIs(t, err, ErrInvalid)
+
+			// verify it again, still valid
+			dbtok, err = Verify(ctx, pool, tok3, MustMatchType("test"))
+			require.NoError(t, err)
+			require.EqualValues(t, 3, dbtok.RefID)
+
+			// can create another multi-use for the same type/ref
+			tok4, err := New(ctx, pool, TokenArgs{
+				Type:      "test",
+				RefID:     3,
+				SingleUse: false,
+				Expiry:    time.Minute,
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, tok4)
+			require.NotEqual(t, tok3, tok4)
+
+			// both are still valid
+			dbtok, err = Verify(ctx, pool, tok3, MustMatchType("test"))
+			require.NoError(t, err)
+			require.EqualValues(t, 3, dbtok.RefID)
+			dbtok, err = Verify(ctx, pool, tok4, MustMatchType("test"))
+			require.NoError(t, err)
+			require.EqualValues(t, 3, dbtok.RefID)
+
+			// create a short-lived multi-use
+			tok5, err := New(ctx, pool, TokenArgs{
+				Type:      "test",
+				RefID:     5,
+				SingleUse: false,
+				Expiry:    time.Second,
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, tok5)
+
+			// let it expire
+			time.Sleep(time.Second + time.Millisecond)
+
+			// it is now invalid
+			_, err = Verify(ctx, pool, tok5, nil)
+			require.ErrorIs(t, err, ErrInvalid)
 		})
 	}
 }
