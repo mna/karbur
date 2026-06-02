868cd5032a905edc54adf56653315771a7fdb3b1
Martin Angers <martin.n.angers@gmail.com>
2026-06-02 11:18:34 -0400

    Add tests for the load middleware

--- accounts/acctmw/accounts_test.go
+++ accounts/acctmw/accounts_test.go
@@ -3,9 +3,11 @@ package acctmw
 import (
 	"fmt"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"codeberg.org/mna/karbur/accounts"
 	"codeberg.org/mna/karbur/pgdb"
@@ -13,6 +15,7 @@ import (
 	"codeberg.org/mna/karbur/server/params"
 	"codeberg.org/mna/karbur/tokens"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/publicsuffix"
 )
 
 type deferHandler struct {
@@ -60,7 +63,7 @@ func setupAccounts(tb testing.TB, pool pgdb.Pool, handlers map[Action]http.Handl
 		}
 		w.WriteHeader(http.StatusNoContent)
 	})))
-	mux.Handle("/load", accts.Login(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/load", accts.Load(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if h := handlers[ActionLoad]; h != nil {
 			h.ServeHTTP(w, r)
 			return
@@ -73,8 +76,31 @@ func setupAccounts(tb testing.TB, pool pgdb.Pool, handlers map[Action]http.Handl
 	return accts, srv
 }
 
-func createAccount(tb testing.TB, srvURL, email, pwd string) {
-	res, err := http.Post(srvURL+"/register", "application/json", strings.NewReader(fmt.Sprintf(`{"email":%q, "password":%q, "password2":%[2]q}`, email, pwd)))
+func newBrowserClient(tb testing.TB) *http.Client {
+	jar, err := cookiejar.New(&cookiejar.Options{
+		PublicSuffixList: publicsuffix.List,
+	})
+	require.NoError(tb, err)
+	return &http.Client{Jar: jar, Timeout: 5 * time.Second}
+}
+
+func createAccountWithClient(tb testing.TB, client *http.Client, srvURL, email, pwd string) {
+	res, err := client.Post(srvURL+"/register", "application/json",
+		strings.NewReader(fmt.Sprintf(`{"email":%q, "password":%q, "password2":%[2]q}`, email, pwd)))
 	require.NoError(tb, err)
 	require.Equal(tb, 204, res.StatusCode)
+}
+
+func createAccount(tb testing.TB, srvURL, email, pwd string) {
+	createAccountWithClient(tb, http.DefaultClient, srvURL, email, pwd)
+}
+
+func doLoginWithClient(tb testing.TB, client *http.Client, srvURL, email, pwd string) {
+	res, err := client.Post(srvURL+"/login", "application/json", strings.NewReader(fmt.Sprintf(`{"email":%q, "password":%q}`, email, pwd)))
+	require.NoError(tb, err)
+	require.Equal(tb, 204, res.StatusCode)
+}
+
+func doLogin(tb testing.TB, srvURL, email, pwd string) {
+	doLoginWithClient(tb, http.DefaultClient, srvURL, email, pwd)
 }
