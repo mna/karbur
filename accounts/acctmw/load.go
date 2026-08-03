5b1e5ff07d5bafea512351c8158a400fa344312d
Martin Angers <martin.n.angers@gmail.com>
2026-08-03 16:02:18 -0400

    Design the session data logic

--- accounts/acctmw/load.go
+++ accounts/acctmw/load.go
@@ -27,6 +27,18 @@ func (a *Accounts) Load(h http.Handler) http.Handler {
 			if tok != nil {
 				ctx := r.Context()
 
+				// TODO: if there is JSON data associated with the token, store it in
+				// acctctx.WithSessionData. Application code can add a middleware to
+				// parse it into a typed struct and store it in the ctx (and set a
+				// default/empty struct value if there is currently no data). But it
+				// needs to be possible to store it back and for this middleware to
+				// catch the update, so maybe a acctctx.WithSessionData always stores a
+				// boxed JSON value (possibly empty), and a acctctx.SetSessionData(v
+				// any) updates it with the JSON-marshaled value of v. Up to the
+				// application logic to call SetSessionData to update it (and it keeps
+				// a flag that it was updated so the middleware knows it has to save it
+				// back).
+
 				if tok.RefID == uuid.Nil {
 					// this is an anonymous session
 					ctx = acctctx.WithSessionID(ctx, tok.Token)
