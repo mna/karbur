61aa3f86cab6f7db5b3022bad8ba2181d6a24650
Martin Angers <martin.n.angers@gmail.com>
2026-08-04 09:32:00 -0400

    Implement the context functions to manipulate session data

--- accounts/acctctx/acctctx.go
+++ accounts/acctctx/acctctx.go
@@ -4,15 +4,18 @@ package acctctx
 
 import (
 	"context"
+	"encoding/json"
 
 	"codeberg.org/mna/karbur/accounts"
+	"codeberg.org/mna/karbur/errors"
 )
 
 type ctxKey int
 
 const (
-	accountKey   = ctxKey(0)
-	sessionIDKey = ctxKey(1)
+	accountKey     = ctxKey(0)
+	sessionIDKey   = ctxKey(1)
+	sessionDataKey = ctxKey(2)
 )
 
 // WithAccount returns a context that holds the specified account. Typically
@@ -40,4 +43,45 @@ func SessionID(ctx context.Context) string {
 	v := ctx.Value(sessionIDKey)
 	ssnID, _ := v.(string)
 	return ssnID
+}
+
+type sessionData struct {
+	data  json.RawMessage
+	dirty bool
+}
+
+// WithSessionData stores the data associated with the current session in the
+// context. This should be the raw data as read from storage (unmodified).
+func WithSessionData(ctx context.Context, data json.RawMessage) context.Context {
+	return context.WithValue(ctx, sessionDataKey, &sessionData{data: data})
+}
+
+// SessionData returns the data associated with the current session, and a
+// boolean indicating if it is dirty (if any changes were made since the call
+// to WithSessionData).
+func SessionData(ctx context.Context) (data json.RawMessage, dirty bool) {
+	v := ctx.Value(sessionDataKey)
+	if ssnData, _ := v.(*sessionData); ssnData != nil {
+		return ssnData.data, ssnData.dirty
+	}
+	return nil, false
+}
+
+// ReplaceSessionData replaces the data associated with the current session
+// with the JSON-marshaled version of p, and marks the session data as dirty.
+// It always assumes that the data changed when this function is called.
+func ReplaceSessionData(ctx context.Context, p any) error {
+	v := ctx.Value(sessionDataKey)
+	ssnData, _ := v.(*sessionData)
+	if ssnData == nil {
+		return errors.New("no existing session data to replace")
+	}
+
+	data, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+	ssnData.data = json.RawMessage(data)
+	ssnData.dirty = true
+	return nil
 }
